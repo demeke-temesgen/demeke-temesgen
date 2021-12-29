8add3c5f564b05273074ba44e2aa1629ec40751e
demeke-temesgen <95958284+demeke-temesgen@users.noreply.github.com>
2021-12-29 17:50:03 +0300

    file_upload.py
    Test

---
+++ file_upload.py
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<form action="file.php" method="post" enctype="multipart/form-data">
+	<input type="file"></input>
+	<p><button type="submit"> Submit</button></p>
+</form>
+</body>
+</html>
